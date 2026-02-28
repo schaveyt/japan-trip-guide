a17b5a4a4bcf4267b35c1d46f58342dc7f5458df
Todd Schavey <tschavey@Todds-MacBook-Pro.local>
2026-02-28 14:32:10 -0600

    chore(03-01): install react-leaflet and add CSS foundation
    - Install react-leaflet@5 and leaflet@1.9.4
    - Import leaflet/dist/leaflet.css in main.jsx after index.css
    - Add .leaflet-container img tile fix to index.css (prevents Tailwind max-width reset from breaking tiles)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
+import 'leaflet/dist/leaflet.css'
 import Home from './pages/Home.jsx'
 import ErrorPage from './pages/ErrorPage.jsx'
 
