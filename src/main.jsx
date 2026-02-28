6055637435daa0d41d4a6af52944299dc7e8f847
Todd Schavey <tschavey@ip-10-100-70-202.ec2.internal>
2026-02-28 18:07:49 -0500

    feat(04-03): register /itinerary, /itinerary/:dayNumber, and /food routes
    - Imports ItineraryPage, DayDetailPage, FoodPage
    - Registers all 5 routes in createBrowserRouter array
    - All routes have errorElement: <ErrorPage />

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,16 @@ import 'leaflet/dist/leaflet.css'
 import Home from './pages/Home.jsx'
 import ErrorPage from './pages/ErrorPage.jsx'
 import MapPage from './pages/MapPage.jsx'
+import ItineraryPage from './pages/ItineraryPage.jsx'
+import DayDetailPage from './pages/DayDetailPage.jsx'
+import FoodPage from './pages/FoodPage.jsx'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/map',
-    element: <MapPage />,
-    errorElement: <ErrorPage />,
-  },
+  { path: '/',                     element: <Home />,          errorElement: <ErrorPage /> },
+  { path: '/map',                  element: <MapPage />,       errorElement: <ErrorPage /> },
+  { path: '/itinerary',            element: <ItineraryPage />, errorElement: <ErrorPage /> },
+  { path: '/itinerary/:dayNumber', element: <DayDetailPage />, errorElement: <ErrorPage /> },
+  { path: '/food',                 element: <FoodPage />,      errorElement: <ErrorPage /> },
 ])
 
 createRoot(document.getElementById('root')).render(
